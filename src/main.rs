586a30bffe1fb0ddae45563626b51c1c7ee65d7f
Gavin Weifert-Yeh <33366876+gavinwy@users.noreply.github.com>
2025-06-10 14:34:18 -0700

    try patching keepass-rs to see if I can get this to work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use service_binding::Binding;
 use ssh_agent_lib::client::connect;
 use keepass::{
     db::NodeRef,
@@ -6,16 +5,40 @@ use keepass::{
     DatabaseKey,
     error::DatabaseOpenError
 };
-use std::fs::File;
 
-let mut file = File::open("tests/test0.kdbx")?;
-let key = DatabaseKey::new().with_password("password");
-fn open_kpdb(key: DatabaseKey, db_file: File) {
-    let db = Database::open(&mut file, key)?;
-
+fn open_kpdb(key: DatabaseKey, db_file_location: String) -> Database {
+    let mut db_file = std::fs::File::open(db_file_location).unwrap();
+    Database::open(&mut db_file, key).unwrap()
 }
 
 
 fn main() {
+    let key = DatabaseKey::new().with_password("password");
+    let db = open_kpdb(key,String::from("tests/test_agent.kdbx"));
 
+    for node in &db.root {
+        match node {
+            NodeRef::Group(g) => {
+                //println!("Saw group '{0}'", g.name);
+            },
+            NodeRef::Entry(e) => {
+                println!("{:?}",e);
+                let title = e.get_title().unwrap_or("(no title)");
+                println!("Entry Title: '{0}'", title);
+            //     for field in &e.fields {
+            //         let val = e.get(field.0).unwrap();
+            //         println!("Field String: '{0}'\nField Value: '{1}'", field.0, val)
+            //     }
+            }
+        }
+    }
+
+    let attachments = db.header_attachments;
+    let mut attachment_count = -1;
+    // for i in attachments {
+    //     let content = i.content;
+    //     attachment_count += 1;
+    //     println!("attachment number '{}'", attachment_count);
+    //     println!("Content:\n'{}'", String::from_utf8(content).unwrap());
+    // }
 }
